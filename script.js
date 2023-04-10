cb192000e6afb56d71bfd9d8cff0eac46c68da07
tajciglar <taj.brzovic@gmail.com>
2023-04-10 12:53:17 +0200

    add button to change the grid

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+function changeGrid(){
+    let width = prompt("Insert width of new grid (e.g.: 10)");
+    let height = prompt("Insert height of the new grid (e.g.: 10)");
+}
+
 const container = document.querySelector("#container");
-container.style.cssText =  "display: grid; width: 100%; height:100%;";
+container.style.cssText =  "display: grid; width: 90%; height:90%;";
 for (let i = 0; i < 16; i++){
     const row = document.createElement('div')
     row.classList.add('row');
-    row.style.cssText = "display: flex;";
+    row.style.cssText = "display: flex; flex: 1";
     container.appendChild(row);
     for (let y = 0; y < 16; y++){
         const box = document.createElement('div');
@@ -12,3 +17,11 @@ for (let i = 0; i < 16; i++){
         row.appendChild(box);
     }
 }
+
+
+const box = document.querySelectorAll(".box");
+box.forEach(box => {
+    box.addEventListener('mousemove', () => {
+        box.style.backgroundColor="red";
+    });
+});
