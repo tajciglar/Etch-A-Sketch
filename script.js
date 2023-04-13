6e6690c4f032f6f1443b7fd0d8597c655ffbc929
tajciglar <taj.brzovic@gmail.com>
2023-04-13 18:15:57 +0200

    add color picker and random colors when hovering over color

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function changeGrid(){
     
     let grid = prompt("Insert a number for the  new grid (e.g.: 10 = 10x10, max is 100)");
 
+    // check if a grid is an number and if its lower than 100
     while(isNaN(grid)) {
         grid = prompt("Please enter an number");
     }
@@ -9,13 +10,16 @@ function changeGrid(){
         grid = prompt("Please enter an number that is less than 100");
     }
 
+
+    // removes previos grid
     const container = document.querySelector("#container");
     const row = document.querySelectorAll(".row");
     row.forEach(row =>{
         row.remove()
         });
-
-    container.style.cssText =  "display: grid; width: 80%; height:80%;";
+    
+    // creates a grid
+    container.style.cssText =  "display: grid;";
     for (let i = 0; i < grid; i++){
         const row = document.createElement('div')
         row.classList.add('row');
@@ -24,29 +28,74 @@ function changeGrid(){
         for (let y = 0; y < grid; y++){
             const box = document.createElement('div');
             box.classList.add('box');
-            box.style.cssText = "border: 1px solid black; width: 100%; height: 100%";
+            box.style.cssText = "border: 1px solid black; width: 100%; height: 100%; background-color: white";
             row.appendChild(box);
         }
     }
-
+    // calls function for hover
     addHover();
+
 }
 
 function addHover () {
     const box = document.querySelectorAll(".box");
-    box.forEach(box => {
-        box.addEventListener('mousemove', () => {
-            box.style.backgroundColor="red";
+    const coloredBoxes = {};
+    const colorPicker = document.getElementById("color-picker");
+    let selectedColor = colorPicker.value;
+
+    colorPicker.addEventListener("input", (event) =>{
+        selectedColor = event.target.value;
+    });
+
+    box.forEach((box, index) => {
+        // color based on the selector
+        box.addEventListener('mouseenter', () => {
+
+            if (coloredBoxes[index]) {
+                // Generate a random RGB color
+                const red = Math.floor(Math.random() * 256);
+                const green = Math.floor(Math.random() * 256);
+                const blue = Math.floor(Math.random() * 256);
+                const color = `rgb(${red}, ${green}, ${blue})`;
+        
+                // Set the box color to the random color
+                box.style.backgroundColor = color;
+              } else {
+                // Set the box color to the color that the user picked
+                box.style.backgroundColor = selectedColor;
+                coloredBoxes[index] = true;
+              }
         });
+    
+            
     });
 }
 
+// clears color from the grid
 function clearColor(){
     const box = document.querySelectorAll(".box");
+
     box.forEach(box => { 
-            box.style.backgroundColor="white";
-        });
+        box.style.backgroundColor="white";
+    });
 }
 
-
+//creates a grid
+const DEFAULT = 64;
+const container = document.querySelector("#container");
+container.style.cssText =  "display: grid; ";
+    for (let i = 0; i < DEFAULT; i++){
+        const row = document.createElement('div')
+        row.classList.add('row');
+        row.style.cssText = "display: flex; flex: 1";
+        container.appendChild(row);
+        for (let y = 0; y < DEFAULT; y++){
+            const box = document.createElement('div');
+            box.classList.add('box');
+            box.style.cssText = "border: 1px solid black; width: 100%; height: 100%; background-color: white";
+            row.appendChild(box);
+        }
+    }
+    //calls a function
+    addHover();
 
